c9bcb85bde0448b53ddcb79e9226c05967688d66
Barbara Gonçalves <barbaragoncalvesbg1@gmail.com>
2020-12-22 16:31:03 -0300

    atualizacao

--- features/webmotorsTests.feature
+++ features/webmotorsTests.feature
@@ -1,12 +1,22 @@
 # language: pt
 
+<<<<<<< Updated upstream
 	Funcionalidade: Verificar se o botão de ofertas funciona 
 	
+=======
+	Funcionalidade: Testar pagina de cadastro do Webmotors e verifica se vai para paragina home
+>>>>>>> Stashed changes
 			
 	@evidencia
 	Cenario: Validar botão ver ofertas
 	Dado que eu navego até a página do webmotors
 	Quando eu clico no botão ver ofertas
 	Entao eu sou direcionado a página de ofertas
-
-		
+	
+	@evidencia
+	Cenario: Validar campo de busca
+	Dado que eu navego até a página do webmotors
+	E eu digito na barra de busca
+	Quando eu clico no botão ver ofertas
+	Então eu sou direcionado a página seguinte
+	
--- src/test/java/stepDefinitions/PesquisaWebmotorsTestsStep.java
+++ src/test/java/stepDefinitions/PesquisaWebmotorsTestsStep.java
@@ -1,26 +1,24 @@
 package stepDefinitions;
 
+import io.cucumber.java.an.E;
+import io.cucumber.java.it.Quando;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Entao;
-import pages.VerOfertasPage;
+import pages.PesquisaWebmotorsPage;
 
 public class PesquisaWebmotorsTestsStep {
-	PesquisaWebmotorsTestsStep pesquisaWebmotors = new PesquisaWebmotorsTestsStep();
+	PesquisaWebmotorsPage pesquisaWebmotors = new PesquisaWebmotorsPage();
 	
-	@Dado("que eu navego até a página do webmotors")
-	public void navegarAtePagina(){
-		pesquisaWebmotors.navegarAtePagina();
-	}
-	@Entao("eu digito na barra de busca")
-	public void clicarVerOfertas() {
-		pesquisaWebmotors.clicarVerOfertas();
+	
+	@E("eu digito na barra de busca")
+	public void digitarCampo() {
+		pesquisaWebmotors.preencheProduto("Jetta");
 	}
 	
-	@E("")
 	
 	@Entao("eu sou direcionado a página seguinte")
 	public void verificarPagina() {
-		pesquisaWebmotors.verificarPagina();
+		pesquisaWebmotors.validaUrlPesquisa();
 		
 	}
 
